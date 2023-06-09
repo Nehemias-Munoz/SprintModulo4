9faec3a3fac48aa30103cdd29a091900617fe4e7
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-06-09 13:07:48 -0400

    Add: documentacion metodos de validacion

--- src/validacion/Validacion.java
+++ src/validacion/Validacion.java
@@ -5,6 +5,14 @@ import java.util.*;
 final public class Validacion {
     static Scanner scan = new Scanner(System.in);
 
+    /**
+     * Metodo que se encarga de validar que el parametro ingresado se encuentre entre los parametros de aceptacion
+     * @param param
+     * @param nombreParam
+     * @param vMax
+     * @param vMin
+     * @return
+     */
     public static String minMaxCaracter(String param,String nombreParam, int vMax, int vMin){
         while (param.length() < vMin ||  param.length() > vMax || param.isBlank()){
             System.out.println("Ingrese el dato "+nombreParam+" con un largo minimo de "+vMin+" y largo maximo de "+vMax);
@@ -13,6 +21,12 @@ final public class Validacion {
         return param;
     }
 
+    /**
+     * Metodo que se encarga de quitar guiones, puntos y k del parametro para validar
+     * si es menor a 99999999, viene vacio o la longitud es mayor al numero maximo
+     * @param param
+     * @return String con el rut validado
+     */
     public static String validarLargoRut(String param){
         if(param.contains("-")){
             param.replace("-","");
@@ -30,6 +44,14 @@ final public class Validacion {
         return param;
     }
 
+    /**
+     * Metodo que valida que se cumplan los valores maximos y minimos en un parametro recibido
+     * @param param
+     * @param nombreParam
+     * @param vMax
+     * @param vMin
+     * @return int con la cantidad correcta
+     */
     public static int validarCantidad(String param,String nombreParam, int vMax, int vMin){
         int paramTemp = Integer.parseInt(param);
         while (paramTemp < vMin ||  paramTemp > vMax ){
@@ -38,6 +60,12 @@ final public class Validacion {
         }
         return paramTemp;
     }
+
+    /**
+     * Metodo estatico que valida si el parametro ingresado se encuentra en la lista
+     * @param param
+     * @return String con el dia correcto
+     */
     public static String validarDiaSemana(String param){
         String[] dias = {"lunes", "martes","miercoles","jueves","viernes","sabado","domingo"};
         List listaDias = Arrays.stream(dias).toList();
